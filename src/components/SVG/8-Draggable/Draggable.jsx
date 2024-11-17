e1ad9c52c55ff032db9a9d29959ef85c793a501a
Marina2022 <marusiiiia@yandex.ru>
2024-11-17 17:05:18 +0300

    drag

--- src/components/SVG/8-Draggable/Draggable.jsx
+++ src/components/SVG/8-Draggable/Draggable.jsx
@@ -14,7 +14,7 @@ const DraggableComp = () => {
     const dragMeRef = useRef()
     const circleMaskRef = useRef()
     const grayRef = useRef()
-    const turbulenceRef = useRef()    
+    const turbulenceRef = useRef()
     const squareRef = useRef()
     const circleRef = useRef()
 
@@ -32,16 +32,25 @@ const DraggableComp = () => {
           gsap.set(grayRef.current, {scale: 1.5, transformOrigin: `${this.x + 250}px ${this.y + 250}px`})
         }
       })
-      
-      Draggable.create(squareRef.current)
-      Draggable.create(circleRef.current)
 
-
-      gsap.to(turbulenceRef.current, {attr: {
-          baseFrequency: 0.02
-        }, repeat: -1, yoyo:true, duration: 2
+      Draggable.create(squareRef.current, {
+        onPress: (e) => {
+          e.preventDefault(); // Предотвращает прокрутку
+        },
       })
-      
+      Draggable.create(circleRef.current, {
+        onPress: (e) => {
+          e.preventDefault(); // Предотвращает прокрутку
+        }
+      })
+
+
+      gsap.to(turbulenceRef.current, {
+        attr: {
+          baseFrequency: 0.02
+        }, repeat: -1, yoyo: true, duration: 2
+      })
+
 
     }, []);
 
@@ -70,7 +79,7 @@ const DraggableComp = () => {
 
         <div className={s.warpWrapper}>
           <h1 className={s.title}>Памагитииии</h1>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
             <defs>
               {/*<filter id="warp-filter-0" bx:preset="warp 1 50 0.1" color-interpolation-filters="sRGB" x="-50%" y="-50%"*/}
               <filter id="warp-filter-0" colorInterpolationFilters="sRGB" x="-50%" y="-50%"
@@ -82,12 +91,13 @@ const DraggableComp = () => {
                   <feMergeNode></feMergeNode>
                   <feMergeNode in="SourceGraphic"></feMergeNode>
                 </feMerge>
-                <feTurbulence ref={turbulenceRef} type="fractalNoise" baseFrequency="0.01" numOctaves="1" result="warp"></feTurbulence>
+                <feTurbulence ref={turbulenceRef} type="fractalNoise" baseFrequency="0.01" numOctaves="1"
+                              result="warp"></feTurbulence>
                 <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="50" in="a"
                                    in2="warp"></feDisplacementMap>
               </filter>
             </defs>
-            <rect className={s.rectWarp} x="122" y="110.58" width="338.34" height="221.75"                  ></rect>
+            <rect className={s.rectWarp} x="122" y="110.58" width="338.34" height="221.75"></rect>
           </svg>
         </div>
 
@@ -107,7 +117,7 @@ const DraggableComp = () => {
 
           <div className="demo">
             <div className="draggables">
-              <div className="drag square" ref={squareRef} ></div>
+              <div className="drag square" ref={squareRef}></div>
               <div ref={circleRef} className="drag circle"></div>
             </div>
             <div className="box"></div>
